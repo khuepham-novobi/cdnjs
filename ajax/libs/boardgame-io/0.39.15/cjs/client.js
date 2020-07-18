8e0ead8d20f38e8fc7fab886cd7abb7a96df0a5e
robocdnjs <cdnjs-github@cloudflare.com>
2020-07-18 06:09:27 +0000

    Add boardgame-io v0.39.15

---
+++ ajax/libs/boardgame-io/0.39.15/cjs/client.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+require('redux');
+require('./turn-order-d6c2e620.js');
+require('immer');
+require('./reducer-76d3a4df.js');
+require('./Debug-1c2055b3.js');
+require('svelte');
+require('flatted');
+require('./ai-1fb7bf89.js');
+require('./initialize-18a8be06.js');
+var client = require('./client-c1f310ea.js');
+
+
+
+exports.Client = client.Client;
---
+++ ajax/libs/boardgame-io/0.39.15/esm/internal.js
@@ -0,0 +1,5 @@
+import './turn-order-dce10a02.js';
+import 'immer';
+export { C as CreateGameReducer, P as ProcessGameConfig } from './reducer-b11048c2.js';
+export { I as InitializeGame } from './initialize-63a95034.js';
+export { A as Async, S as Sync } from './base-c99f5be2.js';
