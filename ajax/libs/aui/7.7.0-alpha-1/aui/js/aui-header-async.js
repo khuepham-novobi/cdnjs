13944265c8aed6779d5c9cb4fe02fafb209537ad
robocdnjs <cdnjs-github@cloudflare.com>
2020-07-28 23:10:07 +0000

    Add aui v7.7.0-alpha-1

---
+++ ajax/libs/aui/7.7.0-alpha-1/aui/js/aui-header-async.js
@@ -0,0 +1,62 @@
+/*!!
+ * @atlassian/aui - Atlassian User Interface Framework
+ * @version v7.7.0-alpha-1
+ * @link https://docs.atlassian.com/aui/latest/
+ * @license SEE LICENSE IN LICENSE.md
+ * @author Atlassian Pty Ltd.
+ */
+// src/js/aui/header-async.js
+(typeof window === 'undefined' ? global : window).__710050254069adf409afc95d0c4e3014 = (function () {
+  var module = {
+    exports: {}
+  };
+  var exports = module.exports;
+  
+  'use strict';
+  
+  Object.defineProperty(exports, "__esModule", {
+      value: true
+  });
+  
+  var _createHeader = __ce1fde0c8a7be6924dfb14c2fd41cad6;
+  
+  var _createHeader2 = _interopRequireDefault(_createHeader);
+  
+  var _skate = __2b4750c1fcf6ac03c025dc51774b3897;
+  
+  var _skate2 = _interopRequireDefault(_skate);
+  
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+  
+  var Header = (0, _skate2.default)('aui-header', {
+      type: _skate2.default.type.CLASSNAME,
+      created: function created(element) {
+          (0, _createHeader2.default)(element);
+      }
+  });
+  
+  exports.default = Header;
+  module.exports = exports['default'];
+  
+  return module.exports;
+}).call(this);
+// src/js/aui-header-async.js
+(typeof window === 'undefined' ? global : window).__0e7463c4caf1173f6d9280f6868e6523 = (function () {
+  var module = {
+    exports: {}
+  };
+  var exports = module.exports;
+  
+  'use strict';
+  
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  
+  __710050254069adf409afc95d0c4e3014;
+  
+  exports.default = window.AJS;
+  module.exports = exports['default'];
+  
+  return module.exports;
+}).call(this);
